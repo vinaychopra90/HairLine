027f5d2170cc9e18c8642b8ecd9213d45232f5c1
Vinay Chopra <vinay.chopra@olacabs.com>
2018-02-27 20:56:09 +0530

    Initial Commit

---
+++ HairLine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HairLine
+//
+//  Created by Vinay Chopra on 27/02/18.
+//  Copyright © 2018 Vinay Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
